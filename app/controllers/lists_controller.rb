81562a528dbc065a06937c7bd92c9dce9ca41f1f
yuuya0061 <258228254+yuuya0061@ users.noreply.github.com>
2026-04-17 07:19:22 +0000

    Finish 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,7 +21,15 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+
   private
+
   def list_params
     params.require(:list).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "lists/edit"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,6 +13,8 @@ Rails.application.routes.draw do
   get 'top'=> 'homes#top'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # Defines the root path route ("/")
   # root "posts#index"
 end
